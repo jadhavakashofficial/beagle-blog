b609ac6a33c994d596f6661f0593c614fafa1b4e
cybersniperop <67035758+cybersniperop@users.noreply.github.com>
2025-03-10 22:44:21 +0530

    Initial commit for Beagle Blog

---
+++ script.js
@@ -0,0 +1,9 @@
+// A simple JavaScript file
+
+// Log a message to the console when the page loads
+console.log("Welcome to the Beagle Blog!");
+
+// Optional: Add a small interactive feature
+window.addEventListener('DOMContentLoaded', () => {
+  // alert("Beagle Blog page loaded!");
+});
